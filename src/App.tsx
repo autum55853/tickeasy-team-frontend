954b3b0dcafe539b2d898561eef24ffc91cc596a
Satoru <adam29457756@gmail.com>
2025-04-15 00:02:55 +0800

    refactor: 路由啟動內容位置 以便所有核心架構內容都core編輯

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
-import { Suspense } from "react";
-import { Routes, Route } from "react-router-dom";
-import { routers } from "@/core/routers";
-import { RouteView } from "@/core/types/router";
-
+// 啟用路由組件
+import Boot from "@/core/boot";
 function App() {
-  return (
-    <div className="h-[100vh] w-[100vw]">
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          {routers.map(({ path, component: Component }: RouteView) => (
-            <Route key={path} path={path} element={<Component />} />
-          ))}
-        </Routes>
-      </Suspense>
-    </div>
-  );
+  return <Boot />;
 }
 
 export default App;
