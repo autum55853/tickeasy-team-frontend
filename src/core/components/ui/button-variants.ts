064be2f3cddc25c770f2160a22cde7b978be1545
Satoru <adam29457756@gmail.com>
2025-04-16 00:12:35 +0800

    feat: 顏色變數配置

--- src/core/components/ui/button-variants.ts
+++ src/core/components/ui/button-variants.ts
@@ -7,10 +7,12 @@ export const buttonVariants = cva(
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline: "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+        outline: "border border-input text-primary bg-background hover:bg-primary hover:text-primary-foreground",
         secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        gradient: "bg-[image:var(--primary-gradient-horizontal)] text-primary-foreground hover:opacity-90",
+        gradientVertical: "bg-[image:var(--primary-gradient-vertical)] text-primary-foreground hover:opacity-90",
       },
       size: {
         default: "h-10 px-4 py-2",
--- src/pages/home/views/page.tsx
+++ src/pages/home/views/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
     <div>
       <Title title="簡單的專案規劃範例-計數器" />
       <div>數字: {count}</div>
-      <div className="flex items-center gap-2">
+      <div className="bg-black-500 flex items-center gap-2">
         <Button variant={"outline"} onClick={increment}>
           +
         </Button>
@@ -16,7 +16,25 @@ export default function Page() {
           -
         </Button>
         <HomeBtn onClick={reset}>Reset</HomeBtn>
+
+        <Button variant={"default"} onClick={decrement}>
+          default
+        </Button>
+        <Button variant={"outline"} onClick={decrement}>
+          outline
+        </Button>
+        <Button variant="gradient">水平渐变按钮</Button>
+        <Button variant="gradientVertical">垂直渐变按钮</Button>
       </div>
+      <div className="flex h-20 w-20 items-center justify-center rounded-full bg-neutral-600">
+        <span className="text-white">top</span>
+      </div>
+      <h1>我是h1</h1>
+      <h2>我是h2</h2>
+      <h3>我是h3</h3>
+      <h4>我是h4</h4>
+      <h5>我是h5</h5>
+      <h6>我是h6</h6>
     </div>
   );
 }
