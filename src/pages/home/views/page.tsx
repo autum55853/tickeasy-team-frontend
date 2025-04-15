ac70598a5531637b973039a82771bbc82d9c057c
Satoru <adam29457756@gmail.com>
2025-04-15 22:50:56 +0800

    feat: demo button 範例

--- src/pages/home/views/page.tsx
+++ src/pages/home/views/page.tsx
@@ -1,15 +1,22 @@
 import useCounter from "../hooks/useCounter";
 import HomeBtn from "../components/homeBtn";
 import Title from "@/core/components/global/title";
+import { Button } from "@/core/components/ui/button";
 export default function Page() {
   const { count, increment, decrement, reset } = useCounter({ initialValue: 7, min: 0, max: 10 });
   return (
     <div>
       <Title title="簡單的專案規劃範例-計數器" />
       <div>數字: {count}</div>
-      <HomeBtn onClick={increment}>+</HomeBtn>
-      <HomeBtn onClick={decrement}>-</HomeBtn>
-      <HomeBtn onClick={reset}>Reset</HomeBtn>
+      <div className="flex items-center gap-2">
+        <Button variant={"outline"} onClick={increment}>
+          +
+        </Button>
+        <Button variant={"outline"} onClick={decrement}>
+          -
+        </Button>
+        <HomeBtn onClick={reset}>Reset</HomeBtn>
+      </div>
     </div>
   );
 }
