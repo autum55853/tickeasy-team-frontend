05e1e33d19e23251a76d5e5e1a8a5a66eb2e7c48
A-Gui <adam29457756@gmail.com>
2025-04-24 13:05:08 +0800

    feat:註冊目前figma規劃的 icons內容

---
+++ src/core/components/global/demoIcon.tsx
@@ -0,0 +1,8 @@
+import { Icon } from '@iconify-icon/react';
+export function Demo() {
+	return (
+		<div className="demo">
+			<Icon icon="my-calendar" style={{ color: "#FF00FF", fontSize: "30px" }}  />
+		</div>
+	);
+}
