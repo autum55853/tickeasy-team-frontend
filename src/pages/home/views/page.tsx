c074380fc6b47258382f8c9367dfb9d1690efda3
A-Gui <adam29457756@gmail.com>
2025-04-26 15:05:26 +0800

    feat: footer layout

--- src/pages/home/views/page.tsx
+++ src/pages/home/views/page.tsx
@@ -7,14 +7,14 @@ import VenueSection from "../components/VenueSection";
 import Footer from "@/core/components/global/footer";
 export default function Page() {
   return (
-    <div className="mt-24 flex h-[calc(100vh-6rem)] flex-col">
+    <div className="mt-24 flex h-[calc(100vh-6rem)] flex-col pb-[400px]">
       <Header />
       <main className="flex-grow">
         <BannerSection />
-        {/* <TrendSection /> */}
-        {/* <LastestSection /> */}
-        {/* <CategorySection /> */}
-        {/* <VenueSection /> */}
+        <TrendSection />
+        <LastestSection />
+        <CategorySection />
+        <VenueSection />
         <Footer />
       </main>
     </div>
