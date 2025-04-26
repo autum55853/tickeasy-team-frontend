d7842124ee2552cdd438fd5760215c6fdff54327
Amberr-Lin <nature55853@gmail.com>
2025-04-26 21:37:27 +0800

    feat: 完成Input component

---
+++ src/core/components/ui/input.tsx
@@ -0,0 +1,28 @@
+import { cn } from "@/core/lib/utils";
+import * as React from "react";
+export interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  type?: string;
+  placeholder?: string;
+  id?: string;
+}
+
+const Input = React.forwardRef<HTMLInputElement, CheckboxProps>(({ id, type, label, placeholder, onChange, ...props }) => {
+  return (
+    <div className={cn("m-3 grid w-full max-w-sm items-center gap-1")} {...props}>
+      <label className="pl-2" htmlFor={id}>
+        {label}
+      </label>
+      <input
+        className="border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
+        type={type}
+        id={id}
+        placeholder={placeholder}
+        onChange={(e) => onChange?.(e)}
+      />
+    </div>
+  );
+});
+Input.displayName = "Input";
+
+export { Input };
