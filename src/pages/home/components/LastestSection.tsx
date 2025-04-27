3b37787df0cb4faa92d32d09e0b42717f1c4f7f5
A-Gui <adam29457756@gmail.com>
2025-04-27 23:27:20 +0800

    style: 最新活動樣式

--- src/pages/home/components/LastestSection.tsx
+++ src/pages/home/components/LastestSection.tsx
@@ -78,7 +78,7 @@ export default function LastestSection() {
       <div className="relative hidden h-[100vh] lg:block">
         <div className="relative mx-auto w-fit text-center text-4xl font-bold select-none">
           <h2 className="relative z-20 bg-gradient-to-r from-[#2D6ED0] to-[#2BC6CC] bg-clip-text text-[48px] text-transparent">最新活動</h2>
-          <span className={`absolute top-[50%] left-1/2 z-10 w-[400px] -translate-x-1/2 text-center text-[40px] text-nowrap text-neutral-200`}>
+          <span className={`absolute top-[50%] left-1/2 z-10 w-[400px] -translate-x-1/2 text-center text-[40px] text-nowrap text-neutral-100`}>
             Latest Events
           </span>
         </div>
