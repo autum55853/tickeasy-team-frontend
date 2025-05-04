790df944146d4b3d0759832a32ddc57850341355
A-Gui <adam29457756@gmail.com>
2025-05-04 15:54:25 +0800

    feat: 會員中心layout (只有input、select)

---
+++ src/pages/user/components/ProfileInfo.tsx
@@ -0,0 +1,221 @@
+import ProfileAvatar from "./ProfileAvatar";
+import DefaultImg from "@/assets/images/bg-user.png";
+import { T_ProfileInfo } from "../types/profileInfo";
+import { useForm } from "react-hook-form";
+import { Input } from "@/core/components/ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/core/components/ui/select";
+import { Button } from "@/core/components/ui/button";
+import { useEffect } from "react";
+
+interface ProfileInfoProps {
+  isEdit: boolean;
+  data: T_ProfileInfo;
+  onSubmit: (data: T_ProfileInfo) => void;
+  onCancel: () => void;
+}
+
+export default function ProfileInfo({ isEdit, data, onSubmit, onCancel }: ProfileInfoProps) {
+  const { register, watch, setValue, handleSubmit } = useForm<T_ProfileInfo>({
+    defaultValues: data,
+  });
+
+  useEffect(() => {
+    // 手动更新所有字段
+    Object.keys(data).forEach((key) => {
+      setValue(key as keyof T_ProfileInfo, data[key as keyof T_ProfileInfo]);
+    });
+  }, [data, setValue]);
+
+  const renderContent = () => (
+    <div className="relative mt-2 min-h-[70vh] lg:mt-0">
+      {isEdit ? (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex w-full flex-col gap-4"
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              e.preventDefault();
+            }
+          }}
+        >
+          <div className="flex h-[40px] items-center">
+            <p className="w-[120px] pr-4 text-right font-bold">帳號</p>
+            <Input
+              id="email"
+              {...register("email")}
+              value={watch("email")}
+              onChange={(e) => setValue("email", e.target.value)}
+              className="max-w-[250px] flex-1 disabled:cursor-not-allowed disabled:opacity-50"
+              disabled
+            />
+          </div>
+          <div className="flex h-[40px] items-center">
+            <p className="w-[120px] pr-4 text-right font-bold">暱稱</p>
+            <Input
+              id="name"
+              {...register("name")}
+              value={watch("name")}
+              onChange={(e) => setValue("name", e.target.value)}
+              className="max-w-[250px] flex-1"
+            />
+          </div>
+          <div className="flex h-[40px] items-center">
+            <p className="w-[120px] pr-4 text-right font-bold">聯絡方式</p>
+            <Input
+              id="phone"
+              {...register("phone")}
+              value={watch("phone")}
+              onChange={(e) => setValue("phone", e.target.value)}
+              className="max-w-[250px] flex-1"
+            />
+          </div>
+          <div className="flex h-[40px] items-center">
+            <p className="w-[120px] pr-4 text-right font-bold">出生年月日</p>
+            <Input
+              id="birthday"
+              type="date"
+              {...register("birthday")}
+              value={watch("birthday")}
+              onChange={(e) => setValue("birthday", e.target.value)}
+              className="max-w-[250px] flex-1"
+            />
+          </div>
+          <div className="flex h-[40px] items-center">
+            <p className="w-[120px] pr-4 text-right font-bold">生理性別</p>
+            <Select
+              value={watch("gender")}
+              onValueChange={(value) => {
+                setValue("gender", value);
+              }}
+            >
+              <SelectTrigger className="ml-2 max-w-[250px] flex-1 text-base text-neutral-600 focus:ring-0 focus:ring-offset-0">
+                <SelectValue placeholder="請選擇性別" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem className="text-base text-neutral-600 focus:bg-slate-100 focus:text-neutral-600" value="男">
+                  男
+                </SelectItem>
+                <SelectItem className="text-base text-neutral-600 focus:bg-slate-100 focus:text-neutral-600" value="女">
+                  女
+                </SelectItem>
+                <SelectItem className="text-base text-neutral-600 focus:bg-slate-100 focus:text-neutral-600" value="其他">
+                  其他
+                </SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="flex h-[40px] items-center">
+            <p className="w-[120px] pr-4 text-right font-bold">偏好活動區域</p>
+            <Input id="preferredRegions" {...register("preferredRegions")} value={watch("preferredRegions")} className="max-w-[250px] flex-1" />
+          </div>
+          <div className="flex h-[40px] items-center">
+            <p className="w-[120px] pr-4 text-right font-bold">偏好活動類型</p>
+            <Input
+              id="preferredEventTypes"
+              {...register("preferredEventTypes")}
+              value={watch("preferredEventTypes")}
+              className="max-w-[250px] flex-1"
+            />
+          </div>
+          <div className="flex h-[40px] items-center">
+            <p className="w-[120px] pr-4 text-right font-bold">國家／地區</p>
+            <Select
+              value={watch("country")}
+              onValueChange={(value) => {
+                setValue("country", value);
+              }}
+            >
+              <SelectTrigger className="ml-2 max-w-[250px] flex-1 text-base text-neutral-600 focus:ring-0 focus:ring-offset-0">
+                <SelectValue placeholder="請選擇國家／地區" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem className="text-base text-neutral-600 focus:bg-slate-100 focus:text-neutral-600" value="台灣">
+                  台灣
+                </SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="flex h-[40px] items-center">
+            <p className="w-[120px] pr-4 text-right font-bold">詳細地址</p>
+            <Input
+              id="address"
+              {...register("address")}
+              value={watch("address")}
+              onChange={(e) => setValue("address", e.target.value)}
+              className="max-w-[250px] flex-1"
+            />
+          </div>
+          <div className="flex justify-end gap-4 lg:mt-4 lg:justify-start lg:pl-28">
+            <Button
+              type="button"
+              variant="outline"
+              onClick={(e) => {
+                e.preventDefault();
+                // 重置表單數據到原始狀態
+                Object.keys(data).forEach((key) => {
+                  setValue(key as keyof T_ProfileInfo, data[key as keyof T_ProfileInfo]);
+                });
+                onCancel();
+              }}
+            >
+              取消編輯
+            </Button>
+            <Button type="submit" variant="default">
+              儲存會員資料
+            </Button>
+          </div>
+        </form>
+      ) : (
+        <div className="flex w-full flex-col gap-4 px-4">
+          <div className="flex h-[40px] items-center">
+            <p className="w-[120px] pr-4 text-right font-bold">帳號</p>
+            <p className="flex-1 text-sm text-gray-500">{data.email}</p>
+          </div>
+          <div className="flex h-[40px] items-center">
+            <p className="w-[120px] pr-4 text-right font-bold">暱稱</p>
+            <p className="flex-1 text-sm text-gray-500">{data?.name || "-"}</p>
+          </div>
+          <div className="flex h-[40px] items-center">
+            <p className="w-[120px] pr-4 text-right font-bold">聯絡方式</p>
+            <p className="flex-1 text-sm text-gray-500">{data?.phone || "-"}</p>
+          </div>
+          <div className="flex h-[40px] items-center">
+            <p className="w-[120px] pr-4 text-right font-bold">出生年月日</p>
+            <p className="flex-1 text-sm text-gray-500">{data?.birthday || "-"}</p>
+          </div>
+          <div className="flex h-[40px] items-center">
+            <p className="w-[120px] pr-4 text-right font-bold">生理性別</p>
+            <p className="flex-1 text-sm text-gray-500">{data?.gender || "-"}</p>
+          </div>
+          <div className="flex h-[40px] items-center">
+            <p className="w-[120px] pr-4 text-right font-bold">偏好活動區域</p>
+            <p className="flex-1 text-sm text-gray-500">{data?.preferredRegions || "-"}</p>
+          </div>
+          <div className="flex h-[40px] items-center">
+            <p className="w-[120px] pr-4 text-right font-bold">偏好活動類型</p>
+            <p className="flex-1 text-sm text-gray-500">{data?.preferredEventTypes || "-"}</p>
+          </div>
+          <div className="flex h-[40px] items-center">
+            <p className="w-[120px] pr-4 text-right font-bold">國家／地區</p>
+            <p className="flex-1 text-sm text-gray-500">{data?.country || "-"}</p>
+          </div>
+          <div className="flex h-[40px] items-center">
+            <p className="w-[120px] pr-4 text-right font-bold">詳細地址</p>
+            <p className="flex-1 text-sm text-gray-500">{data?.address || "-"}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+
+  return (
+    <div className="relative min-h-[70vh] p-4">
+      {/* 會員頭像 */}
+      <div className="flex justify-center lg:justify-start">
+        <ProfileAvatar img={data?.img || DefaultImg} />
+      </div>
+      {/* 會員資訊 */}
+      {renderContent()}
+    </div>
+  );
+}
--- src/pages/user/views/profile.tsx
+++ src/pages/user/views/profile.tsx
@@ -1,3 +1,40 @@
+import { useState } from "react";
+import { Button } from "@/core/components/ui/button";
+import ProfileInfo from "../components/ProfileInfo";
+import { T_ProfileInfo } from "../types/profileInfo";
 export default function Profile() {
-  return <div>我是會員中心</div>;
+  const [isEdit, setIsEdit] = useState(false);
+  const [profileData, setProfileData] = useState<T_ProfileInfo>({
+    email: "adam294577@gmail.com",
+    name: "Adam",
+    phone: "0912345678",
+    birthday: "1990-01-01",
+    gender: "男",
+    preferredRegions: ["台北市"],
+    preferredEventTypes: ["流行"],
+    country: "台灣",
+    address: "台北市中山區",
+    img: "",
+  });
+
+  const handleSubmit = (updatedData: T_ProfileInfo) => {
+    console.log("updatedData", updatedData);
+
+    setProfileData(updatedData);
+    setIsEdit(false);
+  };
+
+  return (
+    <>
+      <div className="mt-10 flex h-[50px] items-center gap-4 lg:mt-0">
+        <h4 className="text-2xl font-bold">基本資料</h4>
+        {!isEdit && (
+          <Button variant="outline" onClick={() => setIsEdit(true)}>
+            修改會員資料
+          </Button>
+        )}
+      </div>
+      <ProfileInfo isEdit={isEdit} data={profileData} onSubmit={handleSubmit} onCancel={() => setIsEdit(false)} />
+    </>
+  );
 }
