37130051aea62e5be0834347b57432c18415cc9f
Amberr-Lin <nature55853@gmail.com>
2025-05-22 22:43:06 +0800

    修正: 1. 將註冊頁面的服務條款與隱私條款, 改以TermDialog顯示對應的內容 2. 調整input元件的錯誤訊息顯示 3. 調整checkbox元件的錯誤訊息顯示

--- src/core/components/ui/input.tsx
+++ src/core/components/ui/input.tsx
@@ -1,37 +1,50 @@
 import { cn } from "@/core/lib/utils";
+import { TriangleAlert } from "lucide-react";
+
 import * as React from "react";
-export interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   type?: string;
   placeholder?: string;
   id?: string;
   error?: boolean;
   errorMessage?: string;
+  labelClass?: string;
   inputClass?: string;
   required?: boolean;
-  value?: string;
 }
 
-const Input = React.forwardRef<HTMLInputElement, CheckboxProps>(
-  ({ id, type, label, placeholder, disabled, maxLength, required, value, onChange, error, errorMessage, inputClass }, ref) => {
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ id, type, label, placeholder, disabled, maxLength, required, error, errorMessage, labelClass, inputClass, className, ...props }, ref) => {
     return (
-      <div className={cn("m-1 h-[80px] w-full")}>
-        <label className="pl-2" htmlFor={id}>
+      <div className={cn("m-1 flex h-[80px] w-full flex-col")}>
+        <label className={`${labelClass} pl-1`} htmlFor={id}>
           {label}
           {required && <span className="ml-1 text-lg text-red-500">*</span>}
         </label>
         <input
-          className={`${inputClass} border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring my-2 w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50`}
+          className={cn(
+            "border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+            error && "border-red-500 focus-visible:ring-red-500",
+            inputClass,
+            className
+          )}
           type={type}
           id={id}
-          value={value}
           placeholder={placeholder}
-          onChange={(e) => onChange?.(e)}
-          ref={ref}
           disabled={disabled}
           maxLength={maxLength}
+          ref={ref}
+          {...props}
         />
-        <p className="pl-2 text-sm text-red-500">{error && errorMessage ? errorMessage : ""}</p>
+        <p className="mt-1 flex items-center gap-1 pl-2 text-start text-sm text-red-500">
+          {error && errorMessage && (
+            <>
+              <TriangleAlert className="h-4 w-4 flex-shrink-0" />
+              {errorMessage}
+            </>
+          )}
+        </p>
       </div>
     );
   }
